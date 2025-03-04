d18138e3a78131048ba5a8f708f67db8bdf77390
Arboleda71 <camiloarboleda7171@gmail.com>
2025-03-03 21:38:16 -0500

    Change 2

--- punto1.py
+++ punto1.py
@@ -1,3 +1,15 @@
+"""📌 Ejercicio 1: Convertidor de Temperatura
+📌 Descripción:
+ ✔ Pedir al usuario una temperatura en grados Celsius.
+ ✔ Convertirla a Fahrenheit y Kelvin.
+ ✔ Mostrar los resultados.
+📌 Fórmulas:
+ ✔ Fahrenheit = (Celsius × 9/5) + 32
+ ✔ Kelvin = Celsius + 273.15
+📌 Ejemplo:
+Ingrese la temperatura en °C: 25  
+🌡️ Fahrenheit: 77°F  
+🌡️ Kelvin: 298.15K """
 
 
 def simulador_cajero():
