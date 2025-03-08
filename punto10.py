71e72760d1e4a54507772dc24b642bb4b16a467b
Arboleda71 <164149180+Arboleda71@users.noreply.github.com>
2025-03-08 16:47:32 -0500

    punto10.py

--- punto10.py
+++ punto10.py
@@ -6,16 +6,16 @@
 Ingrese 5 nombres: ["Pedro", "Ana", "Luis", "Carlos", "Sofía"]  
 ✅ Lista ordenada: ["Ana", "Carlos", "Luis", "Pedro", "Sofía"]"""
 
-def ordenador_nombres():
+def ordenador_nombres(): #Se crea la función
     contador=0
-    array=[]
-    while contador<5:
+    array=[]   #inicializamos un contador y una lista
+    while contador<5: #while que nos asegura que pidan 5 números como lo pedido en el ejercicio
         
         nombres=input("Ingrese el nombre ")
         contador+=1
-        array.append(nombres)
-    array_ordenado=sorted(array)    
-    print(f"✅ Lista ordenada: {array_ordenado}")
+        array.append(nombres)#Se agregan los string ingresados a la lista con la función append
+    array_ordenado=sorted(array)    #Se envían a otra lista ordenada
+    print(f"✅ Lista ordenada: {array_ordenado}") #Se imprime lista
      
 
 ordenador_nombres()
