4f9ab57693d7a6febb7eec35edc52196f73c5701
MF-TR <m.f.t.r96@gmail.com>
2025-03-08 12:41:05 -0500

    Punto 4 terminado

--- punto4.js
+++ punto4.js
@@ -4,16 +4,16 @@
 // ✔ Mostrar su tabla de multiplicar del 1 al 10.
 
 const prompt = require("prompt-sync")();
-let i=1
+let i=1  // Se declaran variables para i, multiplicacion y numero inrgesado.
 let multi
 let numero = parseInt(prompt("Ingrese numero entero positivo: "))
 
-if(isNaN(numero) || numero <0){
-    console.log("Entrada invalida, verifique el dato ingresado")
-}else {
-    console.log(`Tabla de ${numero}`)
-    for(i=1;i<=10;i++){
-        multi = numero*i
-        console.log(`${numero} x ${i }= ${multi} `)
+if(isNaN(numero) || numero <0){  // Se valida que el numero sea un numero y no sea menor que 0.
+    console.log("Entrada invalida, verifique el dato ingresado")  // Se imprime alerta si una las condiciones se cumple.
+}else {  // Si no se cumplen las condiciones ingresa en el ciclo.
+    console.log(`Tabla de ${numero}`)  // Se imprime mensaje con el numero ingresado.
+    for(i=1;i<=10;i++){  // Se hace ciclo desde 1 hasta 10.
+        multi = numero*i // Se multiplica numero * el valor de i y se guarda en la valirable multi.
+        console.log(`${numero} x ${i }= ${multi} `)  // Se imprime la multiplicacion y su resultado.
     }
 }
