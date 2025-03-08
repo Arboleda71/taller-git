99c4c18c95f16d5558f0375c385e85c2e402ecbc
MF-TR <m.f.t.r96@gmail.com>
2025-03-08 14:08:47 -0500

    Punto 9 terminado

--- punto9.js
+++ punto9.js
@@ -1,10 +1,15 @@
-const prompt = require("prompt-sync")();
+const prompt = require("prompt-sync")(); 
 
-let contDados = prompt("Cuantos dados va a lanzar: ")
+let contDados = parseInt(prompt("🎲 ¿Cuántos dados desea lanzar? "))  // Se solicita al usuario cuantosdados quiere lanzar.
 
-let resultados = []
-for (let i = 0; i< contDados; i++){
-    resultados.push(Math.floor(Math.random() * 6) + 1)
+
+if (isNaN(contDados) || contDados <= 0) {   // Se valida que ingrese un numero y que no sea negativo.
+    console.log("⚠️ Entrada inválida. Ingrese un número entero positivo.") // Mensaje de alerta si el dato no es valido.
+} else {
+    let resultados = [] // Se declara array donde se guardaran los valores aleatrios.
+    for (let i = 0; i < contDados; i++) { // Se recorre un ciclo para los dados.
+        resultados.push(Math.floor(Math.random() * 6) + 1)  // Se hacen numeros aleatorios entre 1 y 6.
+    }
+
+    console.log(`🎲 Resultados de los dados: ${resultados.join(", ")}`); // Se imprime resultados de dados almacenados en el array.
 }
-
-console.log(`🎲Resultado de dados:  ${resultados.join(", ")}`)
