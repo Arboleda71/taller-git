2297e226a3678bb17345d7ec58872d35ae558673
Arboleda71 <164149180+Arboleda71@users.noreply.github.com>
2025-03-08 16:45:01 -0500

    punto1.py

--- punto1.py
+++ punto1.py
@@ -11,22 +11,21 @@ Ingrese la temperatura en °C: 25
 🌡️ Fahrenheit: 77°F  
 🌡️ Kelvin: 298.15K """
 
-
 def conversor():
     try:
-        temperatura = int(input("Ingrese la temperatura en °C Celsius: "))
+        temperatura = int(input("Ingrese la temperatura en °C Celsius: "))  #Se pide al usuario una temperatura en grados Celsius.
 
         if temperatura == "":
-            print("Entrada inválida. Debe ingresar un número.")
+            print("Entrada inválida. Debe ingresar un número.") #Se valida con la primer condición que no esté vacío el input
             return
 
         fahrenheit = (temperatura * 9/5) + 32
-        kelvin= temperatura+273.15
+        kelvin= temperatura+273.15           #Se aplica formula 
         print(f"🌡️ Fahrenheit:  {fahrenheit}°F")
-        print(f"🌡️ Kelvin:  {kelvin}°K")
+        print(f"🌡️ Kelvin:  {kelvin}°K")      #Se imprimen resultados 
 
     except ValueError:
-        print("Entrada inválida. Ingrese un número válido.")
+        print("Entrada inválida. Ingrese un número válido.")  #Se imprime entrada invalida si no coicide con los formatos correctos para input
 
 
 conversor()       
