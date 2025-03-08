74c5d9205844cc1025cf2b7d706eeb90184b0a9c
MF-TR <m.f.t.r96@gmail.com>
2025-03-07 22:12:45 -0500

    Punto 5 terminado

--- punto5.js
+++ punto5.js
@@ -11,18 +11,18 @@ let contNumeros = 0
 let contEspacios= 0
 
 
-let frase = prompt("Ingrese un palabra u oracion: ")  // Se pide al usuario ingresar dato
+let frase = prompt("Ingrese un palabra u oracion: ")  // Se pide al usuario ingresar dato.
 
-for (let caracter of frase) {
-    if (caracter >= 'a' && caracter <= 'z' || caracter >= 'A' && caracter <= 'Z') {
-        contLetras++;
-    } else if (caracter >= '0' && caracter <= '9') {
-        contNumeros++;
-    } else if (caracter === ' ') {
-        contEspacios++;
+for (let caracter of frase) {  // Se recorre cada caracter de la frase.
+    if (caracter >= 'a' && caracter <= 'z' || caracter >= 'A' && caracter <= 'Z') {  // Se valida letras entre a y z en mayuscula y miniscula.
+        contLetras++;  // Aumenta contador si la condicion se cumple.
+    } else if (caracter >= '0' && caracter <= '9') {  // Se valida numeros entre 0 y 9.
+        contNumeros++;  // Aumenta contator si la condicion se cumple.
+    } else if (caracter === ' ') {  // Se valida espacio.
+        contEspacios++;  // Aumenta contador si la condicion se cumple.
     }
 }
-console.log(`Total caracteres: ${frase.length}`)
-console.log(`Letras: ${contLetras}`)
-console.log(`Numeros: ${contNumeros}`)
-console.log(`Espacios: ${contEspacios}`)
+console.log(`Total caracteres: ${frase.length}`)  // Se recorre la frase para validar total de caracteres y se imprime.
+console.log(`🆎Letras: ${contLetras}`)  // Se imprimen contadores de letras, numeros y espacios.
+console.log(`9️⃣Numeros: ${contNumeros}`)
+console.log(`❔Espacios: ${contEspacios}`)
