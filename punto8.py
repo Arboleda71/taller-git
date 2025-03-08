6547a250bc5bd0203507e4603e2ea46dfc92f85c
Arboleda71 <164149180+Arboleda71@users.noreply.github.com>
2025-03-08 16:55:33 -0500

    punto8.py

--- punto8.py
+++ punto8.py
@@ -7,12 +7,12 @@ Ingrese un texto: Me gusta programar en Python
 ✅ El texto tiene 5 palabras. """
 
 
-def contador_():
-    parrafo=input("Ingrese un párrafo  ")
-    base_1=parrafo.strip()
-    palabras=base_1.split()
-    cantidad=len(palabras)
-    print(f"✅ El texto tiene {cantidad} palabras.")
+def contador_(): #Creamos la función
+    parrafo=input("Ingrese un párrafo  ") 
+    base_1=parrafo.strip() #Con la función .strip() Quitamos los espacios de error, el primer y último
+    palabras=base_1.split() # El .split() genera espacios o separa las palabras con los espacios
+    cantidad=len(palabras) # Con Len() contamos el número de número de elementos 
+    print(f"✅ El texto tiene {cantidad} palabras.") #Imprimimos 
    
     
 
