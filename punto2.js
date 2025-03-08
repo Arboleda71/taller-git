03aef297707677050703ab0c3364ee63741c41b2
MF-TR <m.f.t.r96@gmail.com>
2025-03-07 22:30:13 -0500

    Punto 2 terminado

--- punto2.js
+++ punto2.js
@@ -5,23 +5,23 @@
 
 const prompt = require("prompt-sync")();
 
-let numero = parseInt(prompt("Ingrese un numero entero positivo: "));
-let resto
-let suma = 0, i=1
+let numero = parseInt(prompt("Ingrese un numero entero positivo: "));  // Se pide numero entero al usuario.
+let resto  // Se declara variable resto para el calculo.
+let suma = 0, i=1  // Se declara variable para sumar los valores y se empieza el ciclo desde el 1.
 
-if(isNaN(numero)|| numero==="" || numero <=0){
-    console.log("Entrada invalida. Verifique que sea un numero positivo o que el valor no quede vacio.")
-    return;
+if(isNaN(numero)|| numero==="" || numero <=0){  // Se valida numero que si sea un numero, que no este vacio y que sea mayor que 0.
+    console.log("Entrada invalida. Verifique que sea un numero positivo o que el valor no quede vacio.")  // Se imprime alerta alguna de las condiciones se cumple.
+    return; 
 }   
 
-for(i=1; i<numero;i++){
-    resto = numero % i
-    if(resto == 0){
-        suma = suma + i
+for(i=1; i<numero;i++){ // Se hace un ciclo desde el 1 hasta el numero ingresado, aumentando de 1 en 1.
+    resto = numero % i  // Se calcula resto del numero con el valor de i.
+    if(resto == 0){  // Se hace condicion para validar si es un divisor propio con el calculo del resto.
+        suma = suma + i // Se suma valor de i si la condicion de cumple.
     }
 }
-if(numero == suma){
+if(numero == suma){  // Se hace condicion para validar si el numero es igual a la suma de i y se imprime mensaje de numero perfecto.
     console.log(`✅ El numero ${numero} es perfecto`)
-}else {
+}else {  // Sino se imprime mensaje de que no es perfecto.
     console.log(`⚕❌ El numero ${numero} no es perfecto`)
 }
